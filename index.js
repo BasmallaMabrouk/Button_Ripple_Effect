e88f765a74046da9156fd2f91f4fd3980d4c8e17
BasmallaMabrouk <basmallamabrouk@gmail.com>
2022-11-21 15:23:06 +0200

    to triggers when the mouse pointer enters the div

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const btnEL =document.querySelector(".btn");
 /* The Document method querySelector() returns the first Element within the document that matches the specified selector
  or group of selectors. 
 If no matches are found, null is returned.*/
+btnEL.addEventListener("mouseover",(event)=>{ 
+    /* triggers when the mouse pointer enters the div element, and its child elements.
+     The mouseenter event is only triggered when the mouse pointer enters the div element
+    The onmousemove event triggers every time the mouse pointer is moved over the div element.*/
+});
