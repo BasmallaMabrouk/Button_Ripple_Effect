52c87d28a93b8a4aec6b63d1ba6310a072c871fd
BasmallaMabrouk <basmallamabrouk@gmail.com>
2022-11-21 15:52:08 +0200

    to appear effect from all sides

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ btnEL.addEventListener("mouseover",(event)=>{
 const y=(event.pageX - btnEL.offsetTop); 
 /*The HTMLElement.offsetLeft read-only property returns the number of pixels that
  the upper left corner of the current element is offset to the left within the*/
+ btnEL.style.setProperty("--xpos",x+"px")
+btnEL .style.setProperty("--ypos",y,+"px")
+/*The CSSStyleDeclaration.setProperty() method interface sets a new value for a property on a CSS
+ style declaration object.*/
 });
