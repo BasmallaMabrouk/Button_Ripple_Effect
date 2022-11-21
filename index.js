6f4b55bd6d765f279f630ad24db1e3415a5d37d4
BasmallaMabrouk <basmallamabrouk@gmail.com>
2022-11-21 15:01:38 +0200

    find btn class

---
+++ index.js
@@ -0,0 +1,4 @@
+const btnEL =document.querySelector(".btn"); 
+/* The Document method querySelector() returns the first Element within the document that matches the specified selector
+ or group of selectors. 
+If no matches are found, null is returned.*/
